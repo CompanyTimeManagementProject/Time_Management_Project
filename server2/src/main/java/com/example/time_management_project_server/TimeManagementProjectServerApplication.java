83665610fde89915e62612bf0a995263148ef3eb
alzhelez <64131387+alzhelez@users.noreply.github.com>
2021-03-02 08:21:52 +0300

    Trying to add server

---
+++ server2/src/main/java/com/example/time_management_project_server/TimeManagementProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.time_management_project_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagementProjectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeManagementProjectServerApplication.class, args);
+    }
+
+}
