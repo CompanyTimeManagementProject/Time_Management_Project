1275323caefc67b3be15922c1fe0026f0c4461cb
alzhelez <64131387+alzhelez@users.noreply.github.com>
2021-04-18 11:11:40 +0300

    Update server part

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: 9000,
+    SQL_CONNECTION_HOST: 'localhost',
+    SQL_CONNECTION_USER: 'root',
+    SQL_CONNECTION_DATABASE: 'tmp',
+    SQL_CONNECTION_PASSWORD: 'root1'
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2')
+const config = require('./config')
+const vars = require('./middlewares/vars')
+const path = require('path')
+const {notificationsTimer} = require('./utils/utls')
+
+
+const connection = mysql.createConnection({
+    host: config.SQL_CONNECTION_HOST,
+    user: config.SQL_CONNECTION_USER,
+    database: config.SQL_CONNECTION_DATABASE,
+    password: config.SQL_CONNECTION_PASSWORD
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Установка объекта подключения к базе в объект req
+app.use(vars(connection))
+
+app.listen(config.PORT, () => {
+    connection.connect(err => {
+        if(err) console.log(err)
+        else {
+            console.log('successful')
+            notificationsTimer(connection)
+        }
+    })
+})
