6bb6ebf240bcdd3f37029780ac647b13603c1cf1
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-07 23:25:42 +0300

    Clear files from db

--- config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    PORT: 9000,
-    SQL_CONNECTION_HOST: 'localhost',
-    SQL_CONNECTION_USER: 'root',
-    SQL_CONNECTION_DATABASE: 'TMProject',
-    SQL_CONNECTION_PASSWORD: '1234'
-}
