ded7e9c685f5d829f351461a3b7ea0eeba0298aa
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-28 10:52:03 +0300

    Added configs

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: 9000,
-    SQL_CONNECTION_HOST: '6666',
-    SQL_CONNECTION_USER: 'root',
-    SQL_CONNECTION_DATABASE: 'company',
-    SQL_CONNECTION_PASSWORD: 'root1'
+    SQL_CONNECTION_HOST: 'us-cdbr-east-03.cleardb.com',
+    SQL_CONNECTION_USER: 'b6b3865c8d0d03',
+    SQL_CONNECTION_DATABASE: 'heroku_6ac585ecb763a96',
+    SQL_CONNECTION_PASSWORD: '0e21be5c'
 }
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const connection = mysql.createConnection({
 })
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.header("Access-Control-Allow-Origin", "https://itimecompanyclient.herokuapp.com/")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
