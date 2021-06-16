041cd9bffef1c1ad38c558b379a3ebd5c30010bf
alzhelez <64131387+alzhelez@users.noreply.github.com>
2021-06-16 18:45:46 +0300

    Добавлены файлы фронта и бэка

---
+++ back-main/Time_Management_Project-backend-main/routes/queries/queries.js
@@ -0,0 +1,16 @@
+const developersQueries = require('./developers_queries')
+const projectsQueries = require('./projects_queries')
+const tasksQueries = require('./tasks_queries')
+const changingDatesQueries = require('./changing_dates_queries')
+const notificationsQueries = require('./notifications_queries')
+const achievementsQueries = require('./achievents_query')
+
+
+module.exports = {
+    developersQueries,
+    projectsQueries,
+    tasksQueries,
+    changingDatesQueries,
+    notificationsQueries,
+    achievementsQueries
+}
