34933825525da01f7d88ad32011856ef8388f02f
Alexandr Smirnov <newlinefu@gmail.com>
2021-06-17 19:07:21 +0300

    fixes

--- services/back-main/config/index.js
+++ services/back-main/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: 9000,
-    SQL_CONNECTION_HOST: 'localhost',
+    SQL_CONNECTION_HOST: 'localhost:3306',
     SQL_CONNECTION_USER: 'root',
     SQL_CONNECTION_DATABASE: 'company',
     SQL_CONNECTION_PASSWORD: 'root1'
